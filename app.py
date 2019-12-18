24e16d9d8fff34bf1391c04f4d9bb38d9a34b508
Romy A. J. van Broekhuijsen <romy.mol.vanbroekhuijsen@gmail.com>
2019-12-18 14:21:51 +0000

    initial commit

---
+++ app.py
@@ -0,0 +1,17 @@
+import os
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+
+app = Flask(__name__)
+
+app.config["MONGO_DBNAME"] = 'carebears_db'
+app.config["MONGO_URI"] = os.environ.get('MONGODB_CONNECTIONSTRING', 'mongodb://localhost') +  "/" + app.config["MONGO_DBNAME"] + "?retryWrites=true&w=majority"
+
+mongo = PyMongo(app) 
+
+if __name__ == '__main__':
+    print(os.environ.get('IP'))
+    app.run(host=os.environ.get('IP', '0.0.0.0'),
+            port=int(os.environ.get('PORT', '8080')),
+            debug=True)
