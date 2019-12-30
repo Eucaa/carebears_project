e0bec6f00562fe58d34e3560c5b4aae2dd429938
Romy A. J. van Broekhuijsen <romy.mol.vanbroekhuijsen@gmail.com>
2019-12-30 15:03:32 +0000

    initial commit about.html, carebear_info.html and home.html

--- app.py
+++ app.py
@@ -8,7 +8,20 @@ app = Flask(__name__)
 app.config["MONGO_DBNAME"] = 'carebears_db'
 app.config["MONGO_URI"] = os.environ.get('MONGODB_CONNECTIONSTRING', 'mongodb://localhost') +  "/" + app.config["MONGO_DBNAME"] + "?retryWrites=true&w=majority"
 
-mongo = PyMongo(app) 
+mongo = PyMongo(app)
+
+@app.route('/')
+def home():
+    return render_template("home.html")
+
+@app.route('/about')
+def about():
+    return render_template("about.html")    
+
+
+@app.route('/get_carebears_collection')
+def get_carebears_collection():
+    return render_template("carebears_collection.html", collections=mongo.db.carebears_collection.find())
 
 if __name__ == '__main__':
     print(os.environ.get('IP'))
